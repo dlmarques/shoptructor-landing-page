3ff001eceed40a7118971fbe1c1c23687b5723bc
dlmarques <daniel.marquesedigital@gmail.com>
2023-01-14 23:04:26 +0000

    fix login redirect

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <h1>Landing Page</h1>
       <div className="card">
-        <a href={import.meta.env.VITE_LOGIN_URL || ""}>
+        <a href={import.meta.env.VITE_LOGIN_URL}>
           <button>Login</button>
         </a>
       </div>
